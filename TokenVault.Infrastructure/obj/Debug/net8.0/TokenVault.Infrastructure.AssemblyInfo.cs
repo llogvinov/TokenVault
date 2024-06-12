6685486bd8af3c5ed0a7ce828568989af7f3d988
Lev Logvinov <llogvinov356@gmail.com>
2024-06-12 21:41:13 +0300

    empty

--- TokenVault.Infrastructure/obj/Debug/net8.0/TokenVault.Infrastructure.AssemblyInfo.cs
+++ TokenVault.Infrastructure/obj/Debug/net8.0/TokenVault.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TokenVault.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+330a4c0a048976fe1b5b0eb5a496736cc5459f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b246df839cb0f0c918fd063e340189a5cde50d5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("TokenVault.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TokenVault.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
