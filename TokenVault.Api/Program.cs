05d97b87d0080030f83233284e920a620c0a735c
Lev Logvinov <llogvinov356@gmail.com>
2024-06-14 20:24:18 +0300

    Added swagger to project

--- TokenVault.Api/Program.cs
+++ TokenVault.Api/Program.cs
@@ -1,3 +1,5 @@
+using System.Reflection;
+using Microsoft.OpenApi.Models;
 using TokenVault.Api;
 using TokenVault.Application;
 using TokenVault.Infrastructure;
@@ -8,6 +10,21 @@ var builder = WebApplication.CreateBuilder(args);
         .AddPresentation()
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
+
+    builder.Services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Version = "v1",
+            Title = "My API",
+            Description = "A simple example ASP.NET Core Web API",
+        });
+
+        // Optionally, you can add XML comments for better documentation
+        var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+        var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+        c.IncludeXmlComments(xmlPath);
+    });
 }
 
 var app = builder.Build();
@@ -16,5 +33,13 @@ var app = builder.Build();
     app.UseAuthentication();
     app.UseAuthorization();
     app.MapControllers();
+
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+        c.RoutePrefix = string.Empty; // Set Swagger UI at app's root
+    });
+
     app.Run();
 }
